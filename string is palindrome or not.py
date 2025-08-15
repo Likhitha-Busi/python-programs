aee200026db894de9455cc827a1bc20363e9b4ad
Likhitha-Busi <likithabusi9@gmail.com>
2025-08-15 12:55:06 +0530

    Add files via upload

---
+++ string is palindrome or not.py
@@ -0,0 +1,6 @@
+#cheack the string is palindrome or not
+text = input("Enter a string: ")
+if text == text[::-1]:
+    print("The string is a palindrome.")
+else:
+    print("The string is not a palindrome.")
